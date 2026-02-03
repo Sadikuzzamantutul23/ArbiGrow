345aca2956ecebc7a38d299b2eda58d56578404c
sadikuzzaman23 <sadikuzzamantutul@gmail.com>
2026-02-03 23:47:03 +0600

    Updated

--- src/component/Style.jsx
+++ src/component/Style.jsx
@@ -4,6 +4,11 @@ const Style = () => {
   return (
     <div>
       <h1>ABCD</h1>
+
+      <div>
+        <p>Paragraph</p>
+        <img src="https://via.placeholder.com/150" alt="image"/>
+      </div>
     </div>
   )
 }
